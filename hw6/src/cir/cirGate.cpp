42c0f6e8c92a268790ba03ea88de1d2c0faf85ef
02stevenyang850527 <b03901101@ntu.edu.tw>
2016-12-02 20:02:58 +0800

    cirg -fanin has bug

--- hw6/src/cir/cirGate.cpp
+++ hw6/src/cir/cirGate.cpp
@@ -53,6 +53,7 @@ CirGate::reportFanout(int level) const
 {
    assert (level >= 0);
 	setGlobalRef();
+	dfsFanout(level, 0, false);
 }
 
 void
@@ -60,7 +61,7 @@ CirGate::dfs4NetList(int& num) const
 {
 	if ( this-> _type == "UNDEF")
 		return;
-	for (unsigned k = 0; k < _fanin.size(); k++){
+	for (unsigned k = 0; k < _fanin.size(); ++k){
 		if (!_fanin[k]->isGlobalRef())
 			_fanin[k]->dfs4NetList(num);
 	}
@@ -68,7 +69,7 @@ CirGate::dfs4NetList(int& num) const
 	cout << "[" << num << "] ";
 	num++;
 	cout << setw(4) << left << _type << _id;
-	for (unsigned k = 0; k < _fanin.size(); k++){
+	for (unsigned k = 0; k < _fanin.size(); ++k){
 		cout << " ";
 		if (_fanin[k]-> getTypeStr() == "UNDEF")
 			cout << "*";
@@ -82,23 +83,52 @@ CirGate::dfs4NetList(int& num) const
 }
 
 void
-CirGate::dfsFanin(int level, int cur, bool inv) const
+CirGate::dfsFanin(int level, int recur, bool inv) const
 {
 	if (level < 0)
 		return;
-	for (int k = 0; k < cur; k++)
+	for (int k = 0; k < recur; ++k)
+		cout << "  ";
+	if (inv)
+		cout << "!";
+	cout << _type << " " << _id << endl;
+	unsigned n = 0;
+	for ( ; n < _fanin.size(); ++n){
+		if (!_fanin[n]->isGlobalRef())
+			_fanin[n]->dfsFanin(level-1, recur+1,isINV(1,n));
+		else{
+			for (int cnt = 0; cnt < recur; ++cnt)
+				cout << "  ";
+			if (isINV(1,n))
+					cout << "!";
+			cout << _fanin[n]-> getTypeStr() << " " << _fanin[n]->getIdNo();
+			if (_fanin[n]->_fanin.size() > 0 && level > 0)
+			if (_fanin[n]->_fanin[0]->isGlobalRef())
+				cout << " (*)";
+			cout << endl;
+		}
+	}
+	set2GlobalRef();
+}
+
+void
+CirGate::dfsFanout(int level, int recur, bool inv) const
+{
+	if (level < 0)
+		return;
+	for (int k = 0; k < recur; k++)
 		cout << "  ";
 	if (inv)
 		cout << "!";
 	cout << _type << " " << _id << endl;
 	set2GlobalRef();
-	for (unsigned k = 0; k < _fanin.size(); k++){
-		if (!_fanin[k]->isGlobalRef())
-			_fanin[k]->dfsFanin(level-1, cur+1,isINV(1,k));
+	for (unsigned k = 0; k < _fanout.size(); ++k){
+		if (!_fanout[k]->isGlobalRef())
+			_fanout[k]->dfsFanout(level-1, recur+1, isINV(0,k));
 		else{
-			for (int k = 0; k < cur; k++)
+			for (int cnt = 0; cnt < recur; ++cnt)
 				cout << "  ";
-			if (isINV(1,k))
+			if (isINV(0,k))
 				cout << "!";
 			cout << _type << " " << _id << " (*)" << endl;
 		}
